5be57669c69feb399fd232d3552356cdd6780761
Ludovic <80395681+Lxdovic@users.noreply.github.com>
2024-01-08 14:43:42 +0100

    merge(scan): create or add product to cart (#84)
    * feat(scan): Added barcode scan
    
    + BarcodeAnalyzer
    + Button that will later add the item to the cart
    
    * feat(scan): Added modal bottom sheet
    
    + Modal bottom sheet for scan forms (needs tweaking & styles)
    
    * feat(scan): Implemented add product to cart form
    
    * fix(api): Fix auth route in WebSecurityConfig
    
    * feat(Scan): Added create product feature
    
    * feat(scan): added create product form
    
    * feat(scan): add cart item feature
    
    * fix(api): revert BASE_URL

--- client/app/src/main/java/com/example/moulamanagerclient/data/network/ApiService.kt
+++ client/app/src/main/java/com/example/moulamanagerclient/data/network/ApiService.kt
@@ -4,32 +4,42 @@ import com.example.moulamanagerclient.data.model.Pagination
 import com.example.moulamanagerclient.data.model.auth.LoginRequest
 import com.example.moulamanagerclient.data.model.auth.LoginResponse
 import com.example.moulamanagerclient.data.model.auth.RegisterRequest
+import com.example.moulamanagerclient.data.model.cartItem.AddProductToCartRequest
 import com.example.moulamanagerclient.data.model.payment.PaymentIntentResponse
+import com.example.moulamanagerclient.data.model.product.CreateProductRequest
 import com.example.moulamanagerclient.data.model.product.ProductResponse
-import com.stripe.android.model.PaymentIntent
 import retrofit2.Response
 import retrofit2.http.*
 
 interface ApiService {
-	@GET(ApiEndpoints.BARCODE)
-	suspend fun getProductsByBarcode(@Path("barcode") barcode: String): Response<ProductResponse>
+    @GET(ApiEndpoints.BARCODE)
+    suspend fun getProductsByBarcode(@Path("barcode") barcode: String): Response<ProductResponse>
 
-	@GET(ApiEndpoints.PRODUCTS)
-	suspend fun getProducts(@Query("page") page: Int, @Query("size") size: Int): Response<Pagination<ProductResponse>>
+    @GET(ApiEndpoints.PRODUCTS)
+    suspend fun getProducts(@Query("page") page: Int, @Query("size") size: Int): Response<Pagination<ProductResponse>>
 
-	@GET(ApiEndpoints.PRODUCTS_BY_NAME)
-	suspend fun searchProductsByName(
-		@Path("name") name: String,
-		@Query("page") page: Int,
-		@Query("size") size: Int
-	): Response<Pagination<ProductResponse>>
+    @POST(ApiEndpoints.PRODUCTS)
+    suspend fun createProduct(@Body request: CreateProductRequest): Response<ProductResponse>
 
-	@POST(ApiEndpoints.LOGIN)
-	suspend fun login(@Body request: LoginRequest): Response<LoginResponse>
+    @GET(ApiEndpoints.PRODUCTS_BY_NAME)
+    suspend fun searchProductsByName(
+        @Path("name") name: String,
+        @Query("page") page: Int,
+        @Query("size") size: Int
+    ): Response<Pagination<ProductResponse>>
 
-	@POST(ApiEndpoints.REGISTER)
-	suspend fun register(@Body request: RegisterRequest): Response<LoginResponse>
+    @POST(ApiEndpoints.LOGIN)
+    suspend fun login(@Body request: LoginRequest): Response<LoginResponse>
 
-	@POST(ApiEndpoints.CREATE_PAYMENT_INTENT)
-	suspend fun createPaymentIntent(): Response<PaymentIntentResponse>
+    @POST(ApiEndpoints.CART_ITEMS_BARCODE)
+    suspend fun addProductToCartItem(
+        @Body request: AddProductToCartRequest,
+        @Path("barcode") barcode: String
+    ): Response<ProductResponse>
+
+    @POST(ApiEndpoints.REGISTER)
+    suspend fun register(@Body request: RegisterRequest): Response<LoginResponse>
+
+    @POST(ApiEndpoints.CREATE_PAYMENT_INTENT)
+    suspend fun createPaymentIntent(): Response<PaymentIntentResponse>
 }
--- client/app/src/main/java/com/example/moulamanagerclient/data/repositories/cartItem/CartItemRepository.kt
+++ client/app/src/main/java/com/example/moulamanagerclient/data/repositories/cartItem/CartItemRepository.kt
@@ -1,5 +1,24 @@
 package com.example.moulamanagerclient.data.repositories.cartItem
 
-class CartItemRepository {
+import com.example.moulamanagerclient.data.model.cartItem.AddProductToCartRequest
+import com.example.moulamanagerclient.data.model.product.ProductResponse
+import com.example.moulamanagerclient.data.network.ApiHelper
+import com.example.moulamanagerclient.data.network.ApiResult
+import com.example.moulamanagerclient.data.network.ApiService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
+class CartItemRepository
+@Inject
+constructor(
+    private val apiService: ApiService
+) {
+    suspend fun addProduct(product: AddProductToCartRequest, barcode: String): ApiResult<ProductResponse> {
+        return withContext(Dispatchers.IO) {
+            ApiHelper.handleApiResponse(
+                request = { apiService.addProductToCartItem(product, barcode) }
+            )
+        }
+    }
 }
