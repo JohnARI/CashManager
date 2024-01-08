7d24cdcd158074dde95f6b8226e0371b980d8417
Lxdovic <ludovicdebever0@gmail.com>
2024-01-08 14:43:17 +0100

    fix(api): revert BASE_URL

--- client/app/src/main/java/com/example/moulamanagerclient/data/network/ApiEndpoints.kt
+++ client/app/src/main/java/com/example/moulamanagerclient/data/network/ApiEndpoints.kt
@@ -1,7 +1,7 @@
 package com.example.moulamanagerclient.data.network
 
 object ApiEndpoints {
-    const val BASE_URL = "http://10.68.251.98:8080"
+    const val BASE_URL = "http://10.0.2.2:8080"
     const val LOGIN = "auth/sign-in"
     const val REGISTER = "auth/sign-up"
     const val PRODUCTS = "products"
