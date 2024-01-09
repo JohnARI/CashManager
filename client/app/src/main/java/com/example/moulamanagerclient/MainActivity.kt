678542a305396aef422d36eade98d5d98c2a8235
Lxdovic <ludovicdebever0@gmail.com>
2024-01-09 11:56:09 +0100

    feat(app): Locked screen orientation to portrait

--- client/app/src/main/java/com/example/moulamanagerclient/MainActivity.kt
+++ client/app/src/main/java/com/example/moulamanagerclient/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.example.moulamanagerclient
 
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.rememberNavController
 import com.example.moulamanagerclient.data.network.AuthInterceptor
 import com.example.moulamanagerclient.shared.MyAppTheme
@@ -13,18 +20,40 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-	@Inject
-	lateinit var authInterceptor: AuthInterceptor
+    @Inject
+    lateinit var authInterceptor: AuthInterceptor
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContent {
-			MoulamanagerclientTheme {
-				MyAppTheme {
-					val navController = rememberNavController()
-					NavigationHost(navController, authInterceptor)
-				}
-			}
-		}
-	}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LockScreenOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+
+            MoulamanagerclientTheme {
+                MyAppTheme {
+                    val navController = rememberNavController()
+                    NavigationHost(navController, authInterceptor)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LockScreenOrientation(orientation: Int) {
+    val context = LocalContext.current
+    DisposableEffect(Unit) {
+        val activity = context.findActivity() ?: return@DisposableEffect onDispose {}
+        val originalOrientation = activity.requestedOrientation
+        activity.requestedOrientation = orientation
+        onDispose {
+            // restore original orientation when view disappears
+            activity.requestedOrientation = originalOrientation
+        }
+    }
+}
+
+fun Context.findActivity(): Activity? = when (this) {
+    is Activity -> this
+    is ContextWrapper -> baseContext.findActivity()
+    else -> null
 }
